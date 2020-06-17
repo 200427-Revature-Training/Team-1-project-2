cda9b8c49ac17389da217d4583a62cc810203dcd
Hallstead <38665384+Hallstead@users.noreply.github.com>
2020-06-17 16:04:20 -0600

    Update login-component.tsx

--- client/src/components/login-components/login-component.tsx
+++ client/src/components/login-components/login-component.tsx
@@ -3,9 +3,30 @@ import React from 'react'
 export const LoginComponent: React.FC = () => {
 
     return (
+        <section>
+            <h1>Your Concert Finder</h1>
+            <div className="col center">
+                <div className='container'>
+                    <form>
 
-        <div>
-            <h1>This is the login component</h1>
-        </div>
+                        <div className='col'>
+
+                        </div>
+                        <br></br>
+                        <div className='col'>
+                            <input type='text' value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Username'>
+                            </input>
+                        </div>
+                        <br></br>
+                        <div className='col'>
+                            <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password'>
+                            </input>
+                        </div>
+                        <br></br>
+                    </form>
+                    <button type='submit' >Login</button>
+                </div>
+            </div>
+        </section>
     )
 }
