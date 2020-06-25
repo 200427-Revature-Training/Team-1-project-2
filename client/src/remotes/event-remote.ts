daabcd111ba94f61d978a530b55a1f83ed34f241
Rclayton999 <64425715+Rclayton999@users.noreply.github.com>
2020-06-25 17:44:30 -0400

    made some changes to concert-details to allow for dummy database reading

--- client/src/remotes/event-remote.ts
+++ client/src/remotes/event-remote.ts
@@ -48,7 +48,7 @@ const bandA:User[] = [{
 
 
 
-const event1:ConcertEventModel[] = [{
+export const event1:ConcertEventModel[] = [{
     eId:0,
     eName:"Tour Of Destruction",
     eDate:new Date(),
