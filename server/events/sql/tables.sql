5bc18612d80c3575884045682362fef605ad3de9
milton-reyes <altf4616@outlook.com>
2020-06-25 10:39:15 -0400

    changes to sql tables to add INSERT statements of dummy data and other table changes. Changes to DaoUtilities to avoid DRY. Changes to Launcher and UserDaoImpl

--- server/events/sql/tables.sql
+++ server/events/sql/tables.sql
@@ -87,11 +87,70 @@ CREATE TABLE events.users (
 
 
 /* ######################################################
- * ######### QUERY AND DUMMY DATA
+ * ######### QUERIES
  */
-SELECT * FROM users u 
-SELECT * FROM user_roles ur 
+SELECT * FROM events.users u 
+SELECT * FROM events.user_roles ur 
 
+
+/* ######################################################
+ * ######### DUMMY DATA
+ * ######################################################
+ */
+INSERT INTO events.users (first_name,last_name,email,username,"password",picture_url,bio,role_id,band_id,favorite_song_id,place_id) VALUES 
+('Ken','Renelt','ken@gmail.com','kRenelt','password','https://avatars2.githubusercontent.com/u/43582922?s=120&v=4','My name is Ken.',3,3,3,NULL)
+,('Ryan','Clayton','ryan@gmail.com','Rclayton999','password','https://avatars2.githubusercontent.com/u/64425715?s=120&v=4','My name is Ryan.',4,NULL,4,NULL)
+,('Milton','Reyes','Milton@gmail.com','milton-reyes','password','https://avatars3.githubusercontent.com/u/64162564?s=120&v=4','Mi nombre es Milton.',3,1,3,NULL)
+,('Jordon','Hill','Hallstead2@gmail.com','Hallstead','password','https://avatars2.githubusercontent.com/u/38665384?s=460&v=4','I like hotdogs.',1,NULL,1,1)
+,('Jackson','Hopkins','Jackson@gmail.com','mechawing','password','https://avatars2.githubusercontent.com/u/60560661?s=60&v=4','My name is Jackson.',2,1,2,NULL)
+;
+
+INSERT INTO events.user_roles ("role") VALUES 
+('fan')
+,('band member')
+,('manager')
+,('admin')
+;
+
+INSERT INTO events.songs (title,url) VALUES 
+('Hotto Dogu song ft. Google Translate','https://www.youtube.com/watch?v=9mD-ZmWuFTQ')
+,('I''m a Little Teapot','https://www.youtube.com/watch?v=B6en-O5yF0o')
+,('i want it that way','https://www.youtube.com/watch?v=4fndeDfaWCg')
+,('Bye Bye Bye','https://www.youtube.com/watch?v=Eo-KmOd3i7s&list=PL2SZp0S34HEJcLxaLkLhLkIEikLzyfVrN&index=1')
+;
+
+INSERT INTO events.places (zip_code,state,city,street_address) VALUES 
+('83440','ID','Rexburg','248 South 3rd West')
+,('33602','FL','Tampa','401 Channelside Dr')
+;
+
+INSERT INTO events.genres (genre) VALUES 
+('Blues')
+,('Jazz')
+,('Rhythm and Blues')
+,('Rock and Roll')
+,('Country')
+,('Rock')
+,('Soul Music')
+,('Dance Music')
+,('Pop Music')
+,('Children''s Music')
+;
+
+INSERT INTO events.events ("name","date",picture_url,description,place_id) VALUES 
+('Kid''s Concert','2020-07-10 18:00:00.000','https://i1.wp.com/nafme.org/wp-content/files/2014/12/kids-concert.jpg?ssl=1','Join Dinnerware and other musical groups as we celebrate kids around the world!',2)
+;
+
+INSERT INTO events.bands ("name",picture_url,genre_id,featured_song_id,genre,favorite_song_id) VALUES 
+('Dinnerware','https://images.crateandbarrel.com/is/image/Crate/BreeTeapotSHF15/?$web_product_hero$&190411134841&wid=625&hei=625',9,2,NULL,NULL)
+,('Backstreet Boys','https://backstreetboys.com/cdn-cgi/image/width=600,quality=75/https://static.wonderfulunion.net/groundctrl/clients/backstreetboys/media/03/11/images/assets/original.xM4tlqOUONL6xs_cS38uy1YTXjy7LAPuv0Fig03ZeNE.png',10,3,NULL,NULL)
+,('Nsync','https://s1.r29static.com/bin/entry/17f/x,80/1946653/image.jpg',10,4,NULL,NULL)
+;
+
+INSERT INTO events.band_event (band_id,event_id) VALUES 
+(1,1)
+,(2,1)
+;
 /* #######################################################
  * ############   ROLES & PERMISSIONS
  */
