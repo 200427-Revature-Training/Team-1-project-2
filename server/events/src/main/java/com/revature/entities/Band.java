cd12c4997d07db1f8e07d69ddbdf42580fb9764d
milton-reyes <altf4616@outlook.com>
2020-06-30 15:51:26 -0400

    changes to the band model relation to genre from genre type to string type

--- server/events/src/main/java/com/revature/entities/Band.java
+++ server/events/src/main/java/com/revature/entities/Band.java
@@ -2,12 +2,9 @@ package com.revature.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -22,8 +19,6 @@ public class Band {
 	@Column(name = "picture_url")
 	private String picture;
 
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "genre_id")
 	private String genre;
 
 	@Column(name = "featured_song")
