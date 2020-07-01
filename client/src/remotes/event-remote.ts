857dddd8df08890b52dafc4abe4e996872221312
kRenelt <43582922+kRenelt@users.noreply.github.com>
2020-07-01 18:38:24 -0500

    need to merge b4 pull

--- client/src/remotes/event-remote.ts
+++ client/src/remotes/event-remote.ts
@@ -22,6 +22,21 @@ export const getEventById = async (eid:string) => {
 
 // working on it
 export const addConcertEvent = async (concert:ConcertEventModel) => {
-    const response = await internalAxios.get<ConcertEventModel[]>('/events/add');
+    const response = await internalAxios.post<ConcertEventModel[]>('/events');
     return response;
+}
+interface createEvent
+{
+    "id": 2,
+    "name": "No Kid's ALLOWED!",
+    "date": "2020-07-24T23:00:00.000+00:00",
+    "picture": "https://i1.wp.com/nafme.org/wp-content/files/2014/12/kids-concert.jpg?ssl=1",
+    "description": "Join team 1 in celebrating end of training",
+    "place": {
+        "id": 2,
+        "zipCode": 33602,
+        "state": "FL",
+        "city": "Tampa",
+        "streetAddress": "401 Channelside Dr"
+    }
 }
