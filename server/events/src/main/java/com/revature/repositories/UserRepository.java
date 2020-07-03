0a098d6c105834dc444cfe6c11bd3e5b9ceb8472
Rclayton999 <64425715+Rclayton999@users.noreply.github.com>
2020-07-03 19:41:10 -0400

    added user list to event details

--- server/events/src/main/java/com/revature/repositories/UserRepository.java
+++ server/events/src/main/java/com/revature/repositories/UserRepository.java
@@ -1,10 +1,13 @@
 package com.revature.repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.revature.entities.User;
+import com.revature.entities.User_First_Last_Dto;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
@@ -15,5 +18,7 @@ public interface UserRepository extends JpaRepository<User, Integer>{
 	@Query("From User u WHERE :username = u.userName")
 	User login(String username);
 
-
+	//@Query(value="select u.first_name ,u.last_name from events.user_event ue JOIN events.users u ON ue.user_id = u.id WHERE ue.event_id  = ?1", nativeQuery=true)
+	@Query(value="select u.id ,u.first_name, u.last_name,u.email,u.username,u.password, u.picture_url,u.bio ,u.role_id,u.favorite_band,u.favorite_song,u.city  ,u.favorite_genre    ,u.state  from events.user_event ue JOIN events.users u ON ue.user_id = u.id WHERE ue.event_id  = ?1" ,nativeQuery=true)
+	Collection<User> getEventsUsers(int id);
 }
