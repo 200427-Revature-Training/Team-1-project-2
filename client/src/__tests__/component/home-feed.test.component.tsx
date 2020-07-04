053286d99e97a7f4d48143bd7487f25a77cf12e8
kRenelt <43582922+kRenelt@users.noreply.github.com>
2020-07-04 15:10:37 -0500

    working with testing and clean up new enent model

--- client/src/__tests__/component/home-feed.test.component.tsx
+++ client/src/__tests__/component/home-feed.test.component.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import  { HomeComponent } from '../../components/home-components/home-feed-component';
-import { RouteComponentProps } from 'react-router';
 
 
 const routeComponentPropsMock = {
@@ -14,6 +13,12 @@ describe('home-feed.component', () => {
    test('basic render test', () => {
 
        const wrapper = mount(<HomeComponent {...routeComponentPropsMock}/>)
-       expect(wrapper).toBeDefined();
-   }) 
+       expect(wrapper).toMatchSnapshot();
+   });
+
+   test('basic render test', () => {
+
+    const wrapper = mount(<HomeComponent {...routeComponentPropsMock}/>)
+    expect(wrapper).toBeDefined();
+  }) 
 });
